421527728dd21166620dec74a3745d70ac0186e8
Radoslaw <r.szulc.762@studms.ug.edu.pl>
2023-01-08 20:53:58 +0100

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [cityName, setCityName] = useState('London');
+    let [region, setRegion] = useState();
+    let [country, setCountry] = useState();
+    let [localTime, setLocalTime] = useState();
+    var map;
+
+    const getCity = () => {
+        axios.get("http://api.weatherapi.com/v1/current.json?key=8bfae888c3cb4c68853121709230601&q="+cityName+"&aqi=no")
+            .then(result => {
+
+                var lat = result.data.location.lat;
+                var lon = result.data.location.lon;
+                setRegion(result.data.location.region);
+                setCountry(result.data.location.country);
+                setLocalTime(result.data.location.localtime);
+                console.log(country)
+                var L = window.L;
+                var container = L.DomUtil.get('map');
+                if(container != null){
+                    container._leaflet_id = null;
+                }
+                map = L.map('map');
+                map.setView([lat, lon], 8);
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 20,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
+            });
+    }
+
+    return (
+        <div id="app" className="container my-3 d-flex flex-column">
+            <h3>CoolMap</h3>
+
+            <div className='parent-element'>
+                <div id="map" className={"leaflet-container"}></div>
+                <div className='cityStatus'>
+                    <table className='table'>
+                        <thead>
+                        <tr>
+                            <th scope='col'>City:</th>
+                            <th scope="col">{cityName}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Region:</td>
+                            <td>{region}</td>
+                        </tr>
+                        <tr>
+                            <td>Country:</td>
+                            <td>{country}</td>
+                        </tr>
+                        <tr>
+                            <td>Local Time:</td>
+                            <td>{localTime}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className="card mt-3">
+                <div className="card-header">Search the city</div>
+                <div className="card-body">
+                    <div className="input-group input-group-sm">
+                        <input
+                            type="text"
+                            className="form-control ms-2"
+                            placeholder="Y"
+                            onChange={(e) => setCityName(e.target.value)}
+                            value={cityName}
+                        />
+                        <div className="input-group-append">
+                            <button className="btn btn-sm btn-primary" onClick={getCity}>
+                                Show
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
